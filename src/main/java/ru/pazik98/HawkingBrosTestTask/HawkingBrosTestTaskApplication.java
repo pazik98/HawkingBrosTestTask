1236d87446ef7f2361dc6aef71a8b67fa75acfa9
pazik98 <nefedow98@mail.ru>
2024-09-12 16:12:14 +0300

    Init

---
+++ src/main/java/ru/pazik98/HawkingBrosTestTask/HawkingBrosTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.pazik98.HawkingBrosTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawkingBrosTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HawkingBrosTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pazik98/HawkingBrosTestTask/HawkingBrosTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pazik98.HawkingBrosTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HawkingBrosTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
